7d0f930c1b5732fd93b20b3a7fea8685eed56a45
stormymayday <stormymayday7@gmail.com>
2023-08-22 20:01:54 -0700

    Added the '.img' class o the img

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,7 +1,7 @@
 const Person = ({ image, name, age }) => {
     return (
         <article>
-            <img src={image} alt={name} />
+            <img className="img" src={image} alt={name} />
             <div>
                 <h4>{name}</h4>
                 <p>{age} years</p>
