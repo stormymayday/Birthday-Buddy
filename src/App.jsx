b0cd8c1ec66733c775787576031fb42326b07d82
stormymayday <stormymayday7@gmail.com>
2023-12-20 18:58:37 -0800

    Added 'clear' list button

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,20 @@ import List from "./components/List";
 
 const App = () => {
     const [people, setPeople] = useState(data);
-    return <List people={people} />;
+    return (
+        <main>
+            <section className="container">
+                <h3>{people.length} birthdays today</h3>
+                <List people={people} />
+                <button
+                    type="button"
+                    className="btn btn-block"
+                    onClick={() => setPeople([])}
+                >
+                    clear
+                </button>
+            </section>
+        </main>
+    );
 };
 export default App;
